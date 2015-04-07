bd2fb6fe91c148788ea14bcbdef2ad4cb21783aa
Meah-N02223539 <N02223539@hawkmail.newpaltz.edu>
2015-04-07 11:49:40 -0400

    main now uses the gps to take a picture if (lat,long) is greater that (40,-75)

--- code/main.py
+++ code/main.py
@@ -1,13 +1,21 @@
 import camera
-import gps
+from gps import *
+from time import *
+import threading
+import time
 import datetime
 import database
 import os
+#import GpsPoller
 
 
 camera = camera.Camera()
-gps = gpsPi.Gps()
+#gps = gpsPi.Gps()
 db = database.Database('../www/database/pi_database.db')
+
+gpsd = None
+
+os.system('clear')
 
 # camera
 def triggerCamera(gpsData):
@@ -16,7 +24,7 @@ def triggerCamera(gpsData):
 
     dateString = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
-    gpsDataFormated = formatGpsData(gpsData)
+    #gpsDataFormated = formatGpsData(gpsData)
 
     # this section should be uncomented when the method formatGpsData is working
     # if gpsData:
@@ -73,6 +81,44 @@ def formatGpsData(gpsData):
     gpsDataFormated['altitude'] = gpsData['altitude'] + '/10'
     return gpsDataFormated
 
+
+class GpsPoller(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        global gpsd
+        gpsd = gps(mode=WATCH_ENABLE)
+        self.current_value = None
+        self.running = True
+	
+    def run(self):
+        global gpsd
+        while gpsp.running:
+            gpsd.next()
+
+
+if __name__ == '__main__' :
+    gpsp = GpsPoller()
+    waypoint_a = {'latitude' : 41.56654, 'longitude': -74.0547, 'altitude' : 182}
+    try:
+        gpsp.start()
+        while True:
+
+            os.system('clear')
+            gpsData = {'latitude' : gpsd.fix.latitude, 'longitude': gpsd.fix.longitude, 'altitude' : gpsd.fix.altitude}	# unformated gps data 	
+            if(gpsData['latitude'] > 40.00):  #waypoint_a['latitude']):
+                if(gpsData['longitude'] > -75.00): #waypoint_a['longitude']):
+                    triggerCamera(gpsData)
+                    print "Camera Triggered"
+                            
+
+    except (KeyboardInterrupt, SystemExit):
+        print "\nKilling Thread..."
+        gpsp.running = False
+        gpsp.join()
+        print "Done. \nExiting."
+
+
+
 # TODO gps reading
-gpsData = gps.read()
-triggerCamera(gpsData)
+#gpsData = {
+#triggerCamera(gpsData)
