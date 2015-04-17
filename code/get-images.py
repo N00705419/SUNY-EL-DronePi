8cb8eae2386e0492d376986f5e3350520224c547
Frederico Castro <castro.112358@gmail.com>
2015-04-16 21:30:20 -0400

    changed get-images code to suit uwcgi

--- code/get-images.py
+++ code/get-images.py
@@ -6,29 +6,17 @@ def dict_factory(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-
-connection = sqlite3.connect("./database/pi_database.db")
-connection.row_factory = dict_factory
-
-cursor = connection.cursor()
-
-cursor.execute("SELECT * FROM images DESC")
-
-# fetch all or one we'll go for all.
-
-results = cursor.fetchall()
-
-print results
-
-connection.close()
-
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
 
-    html = "<h1>Hello World From Python</h1>\n"
-    html += "<table>\n"
-    for k in env:
-        html += "<tr><td>{}</td><td>{}</td></tr>\n".format(k, env[k])
-    html += "</table>\n"
+    connection = sqlite3.connect("../www/database/pi_database.db")
+    connection.row_factory = dict_factory
 
-    return html
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM images DESC")
+
+    results = cursor.fetchall()
+    connection.close()
+
+    return results
