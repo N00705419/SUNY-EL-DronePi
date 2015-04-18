4bd7a8e32fdc185aee12fd34539b5a13c2ffacfe
Frederico Castro <castro.112358@gmail.com>
2015-04-18 18:21:28 -0400

    php db request working

---
+++ www/get-images.php
@@ -0,0 +1,25 @@
+
+<?php
+
+$db = new PDO('sqlite:database/pi_database.db');
+
+$result = $db->query('SELECT * FROM images');
+
+$data = array();
+
+$result->setFetchMode(PDO::FETCH_ASSOC);
+
+while ($row = $result->fetch()) {
+
+extract($row);
+
+$data[] = array($dateTime, $path_to_image, $latitude, $longitude, $altitude);
+}
+
+$data = json_encode($data);
+
+echo($data);
+
+?>
+
+
